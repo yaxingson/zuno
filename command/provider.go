595e97a0381a994ff5dfa7423c62abc81a7ee716
soetas <3228891558@qq.com>
2025-11-21 11:10:44 +0800

    init project

--- command/provider.go
+++ command/provider.go
@@ -2,15 +2,63 @@ package command
 
 import "github.com/spf13/cobra"
 
+type Provider struct {
+	name    string
+	baseUrl string
+	models  []string
+	apiKey  string
+}
+
+type ProviderArray []Provider
+
+func (arr *ProviderArray) Add(provider Provider) {}
+func (arr *ProviderArray) Del(provider Provider) {}
+func (arr *ProviderArray) List()                 {}
+
 var ProviderCmd = &cobra.Command{
 	Use: "provider",
 }
 
 func init() {
+	providers := ProviderArray{
+		Provider{
+			name:    "openrouter",
+			baseUrl: "https://openrouter.ai/api",
+			models:  []string{},
+			apiKey:  "",
+		},
+		Provider{
+			name:    "aihubmix",
+			baseUrl: "https://aihubmix.com",
+			models:  []string{},
+			apiKey:  "",
+		},
+		Provider{
+			name:    "gptsapi",
+			baseUrl: "https://api.gptsapi.net",
+			models:  []string{},
+			apiKey:  "",
+		},
+		Provider{
+			name:    "dashscope",
+			baseUrl: "https://dashscope.aliyuncs.com/compatible-mode",
+			models:  []string{},
+			apiKey:  "",
+		},
+		Provider{
+			name:    "ark",
+			baseUrl: "https://ark.cn-beijing.volces.com/api",
+			models:  []string{},
+			apiKey:  "",
+		},
+	}
+
 	ProviderCmd.AddCommand(&cobra.Command{
 		Use:  "list",
 		Long: "List all the model providers",
-		Run:  func(cmd *cobra.Command, args []string) {},
+		Run: func(cmd *cobra.Command, args []string) {
+			providers.List()
+		},
 	})
 
 	ProviderCmd.AddCommand(&cobra.Command{
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module zuno
 go 1.16
 
 require (
-	github.com/spf13/cobra v1.10.1 // indirect
+	github.com/spf13/cobra v1.10.1
 	github.com/spf13/pflag v1.0.10 // indirect
 )
